38120d8067e1cc1a1bf7953c141a0fd3e41fead6
Maxim <gtnskatevideo@gmail.com>
2025-10-20 15:45:12 +0300

    Last commit

---
+++ 02. Variables/02_multiple_vars/02_multiple_vars.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Declare and initialize with var with explicit type
+	var coffeeName string = "Espresso"
+
+	// Type inferred
+	var size = "Small"
+
+	// Short declaration and initialization
+	price := 2.50
+
+	fmt.Println("Medium Espresso price is $2.50")
+	fmt.Println(size, coffeeName, "price is $", price)
+}
