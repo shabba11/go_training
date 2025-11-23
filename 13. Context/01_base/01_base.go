a1ff3fc434a6520a923317644cebceb7c792be09
shabba11 <gtnskatevideo@gmail.com>
2025-11-23 23:01:33 +0300

    context finish

--- 13. Context/01_base/01_base.go
+++ 13. Context/01_base/01_base.go
@@ -7,11 +7,14 @@ package main
 import (
 	"context"
 	"fmt"
+	"runtime"
+	"sync"
 	"time"
 )
 
 func main() {
 	BaseKnowledge()
+	workerPool()
 }
 
 func BaseKnowledge() {
@@ -39,4 +42,58 @@ func BaseKnowledge() {
 	withTimeOut, cancel := context.WithTimeout(ctx, time.Second*3) // Тоже самое но не со временем завершения а через сколько секунд завершить.
 	defer cancel()
 	fmt.Println(<-withTimeOut.Done())
+}
+
+func workerPool() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	wg := &sync.WaitGroup()
+	numbersToProcess, processedNumbers := make(chan int, 5), make(chan int, 5)
+	// numbersToProcess - в этом канале будет лежать числа которые нужно обработать,
+	// processedNumbers - числа которые мы уже обработали
+
+	for i := 0; i < runtime.NumCPU(); i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			worker(ctx, numbersToProcess, processedNumbers)
+		}()
+	}
+
+	go func() {
+		for i := 0; i < 1000; i++ {
+			if i == 500 {
+				cancel()
+			}
+			numbersToProcess <- i
+		}
+		close(numbersToProcess)
+	}()
+
+	go func() {
+		wg.Wait()
+		close(processedNumbers)
+	}()
+
+	var counter int
+	for resultValue := range processedNumbers {
+		counter++
+		fmt.Println(resultValue)
+	}
+}
+
+func worker(ctx context.Context, toProcess <-chan int, processed chan<- int) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case value, ok := <-toProcess:
+			if !ok {
+				return
+			}
+			time.Sleep(time.Millisecond)
+			processed <- value * value
+		}
+	}
 }
